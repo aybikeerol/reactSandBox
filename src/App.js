42dcf5d66b5a4316dd03e2f4001e207260d5e6b4
aybikeerol <aybikeerol.01@gmail.com>
2020-05-19 01:58:20 +0300

    secondcommit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
   return (
     <Container fluid>
       <div className="App">
-        <Button className="btn btn-success btn-sm"> asdasd </Button>
+        <Button className="btn btn-success btn-sm"> sdfsdfsasdasd </Button>
         <h1>Hello CodeSandbox</h1>
         <h2>Start editing to see some magic happen!</h2>
       </div>
